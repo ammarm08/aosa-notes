352c5c5aa3ec2a5a0a6554934edb2eb969e258ee
ammarm08 <ammar.mian08@gmail.com>
2017-07-05 18:13:10 -0400

    ci: dispatcher receives commit ids

---
+++ 500-lines/002-continuous-integration/dispatcher.py
@@ -0,0 +1,77 @@
+'''
+Dispatcher.
+
+Observer -> Dispatcher: "You alive?"
+Dispatcher -> Observer: "Yes"
+
+Observer -> Dispatcher: "this is the latest commit"
+
+Dispatcher enqueues commits for "dispatching" to do things (in this case, tests)
+
+For now, dispatcher will acknowledge that it received a message from the Observer
+'''
+
+import argparse
+import re
+import socket
+import SocketServer
+import time
+
+# extends TCPServer to handle multithreading and track some state for us
+class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    runners = []
+    dead = False
+    dispatched_commits = {}
+    pending_commits = []
+
+# request handler: pull 1 MB at a time, match the data against expected actions
+class DispatcherHandler(SocketServer.BaseRequestHandler):
+    command_re = re.compile(r"(\w+)(:.+)*")
+    BUF_SIZE = 1024
+
+    def handle(self):
+        self.data = self.request.recv(self.BUF_SIZE).strip()
+        command_groups = self.command_re.match(self.data)
+
+        if not command_groups:
+            self.request.sendall('Invalid command')
+            return
+        
+        command = command_groups.group(1)
+        if command == 'status':
+            print 'in status'
+            self.request.sendall('OK')
+        elif command == 'dispatch':
+            print 'going to dispatch'
+            commit_id = command_groups.group(2)[1:]
+            self.request.sendall('OK')
+            print 'received and processing commit: %s' % commit_id
+        else:
+            self.request.sendall('Invalid command')
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host',
+                        help = 'dispatcher host, by default uses localhost',
+                        default = 'localhost',
+                        action = 'store')
+
+    parser.add_argument('--port',
+                        help = 'dispatcher port, by default it uses 8888',
+                        default = 8888,
+                        action = 'store')
+
+    return parser.parse_args()
+
+def serve():
+    args = parse_args()
+    server = ThreadingTCPServer((args.host, int(args.port)), DispatcherHandler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    serve() 
+
+
+
+
+
--- 500-lines/002-continuous-integration/observer.py
+++ 500-lines/002-continuous-integration/observer.py
@@ -9,14 +9,10 @@ TBD: dispatch + test-runner + reporter
 
 import argparse
 import os
-# import re
 import socket
-# import SocketServer
 import subprocess
-# import sys
 import time
-
-# import helpers
+import helpers
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -38,34 +34,39 @@ def poll():
     dispatcher_host, dispatcher_port = args.dispatcher_server.split(':')
 
     while True:
+        # delegates finding latest commit to a bash script
         try:
             subprocess.check_output(['./update_repo.sh', args.repo])
         except subprocess.CalledProcessError as e:
             raise Exception('Could not update and check repo. Reason: %s' % e.output)
 
+        # if commit found ...
         if os.path.isfile('.commit_id'):
             with open('.commit_id', 'r') as f:
                 commit = f.readline()
             print 'Current commit: %s' % commit
-            
-            # try:
-            #     response = helpers.communicate(dispatcher_host, int(dispatcher_port), 'status')
-            # except socket.error as e:
-            #     raise Exception('Could not communicate with dispatcher server %s' % e)
 
-            # if response == 'OK':
-            #     commit = ''
-            #     with open('.commit_id', 'r') as f:
-            #         commit = f.readline()
+            # ping the dispatcher (handle errors)
+            try:
+                response = helpers.communicate(dispatcher_host, int(dispatcher_port), 'status')
+            except socket.error as e:
+                raise Exception('Could not communicate with dispatcher server %s' % e)
 
-            #     response = helpers.communicate(dispatcher_host, int(dispatcher_port), 'dispatch:%s' % commit)
-            #     if response != "OK":
-            #         raise Exception("Could not dispatch the test: %s" % response)
+            if response == 'OK':
+                commit = ''
+                with open('.commit_id', 'r') as f:
+                    commit = f.readline()
 
-            #     print "dispatched!"
-            # else:
-            #     raise Exception("Could not dispatch the test: %s" % response)
+                # tell dispatcher about latest commit (handle errors)
+                response = helpers.communicate(dispatcher_host, int(dispatcher_port), 'dispatch:%s' % commit)
+                if response != "OK":
+                    raise Exception("Could not dispatch the test: %s" % response)
+
+                print "dispatched!"
+            else:
+                raise Exception("Could not dispatch the test: %s" % response)
         
+        # sleep for 5 and try again
         time.sleep(5)
 
 if __name__ == "__main__":
